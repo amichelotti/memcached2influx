ef20848de91e94144cd1dc0572e1f423bf1ecf17
rsellari <riccardo.sellari@lnf.infn.it>
2023-03-10 13:46:03 +0100

    added refresh rate parameter

--- memcached2Influx.py
+++ memcached2Influx.py
@@ -6,6 +6,7 @@ from pymemcache.client import base
 import os
 import re
 import json
+import time
 
     
 #Kiwi
@@ -28,9 +29,10 @@ def jsonData2Influx(fileData,clientMemcached):
     fileData['memcached'] = json.loads(jsonData)
     
     if(fileData["variables"]):
-        print (fileData["variables"])
+        print(fileData['memcached'])
+        print(type(fileData['memcached']))
     payload.append({
-            "measurement": "dafneStat",
+            "measurement": fileData["name"],
             "tags": {
                 "key": fileData["keybind"]
             },
@@ -48,13 +50,14 @@ def findTheKey(configFile):
         #knowing that the keybind is always the last value, the return is set right after it
         if(match):
             #match = re.search(pattern,line)
-            parameter = re.sub(r',|"|( ←)',"",match.group(2))
+            parameter = re.sub(r'(,|"|( ←))(?![^\[\]]*\])',"",match.group(2))
             data[match.group(1)] = parameter
         elif(line.find('keybind') != -1):
             line = re.sub(r',|( ←)',"",line)
             pattern = r'"(.+)":"(.+)"'
             match = re.search(pattern,line)
             data[match.group(1)] = match.group(2)
+            data['name'] = data['name'][0:-1]
             return data
         
 
@@ -70,6 +73,8 @@ parser.add_argument("-f", "--file",required = False, help = "the configuration f
 parser.add_argument("-n", "--name",required = False, help = "if you're not using a configuration file, use this parameter to chose the name of the measurement")
 parser.add_argument("-k", "--key",required = False, help = "the key needed to find the data in the memcached DB")
 parser.add_argument("-po", "--port", help = "the port associated with the server address")
+parser.add_argument("-r", "--rate", required = False, default = 5, help = "specify the seconds between 2 different push in the influx db, use this option only if you're not using a configuration file")
+
 
 args = parser.parse_args()
 
@@ -89,15 +94,22 @@ try:
     clientMemcached = base.Client( ("192.168.198.20" , 11211) , connect_timeout=20.0)
 except:
     sys.exit('Cannot reach the memcached server, try again later')
-    
+
 
 if args.key:
-    payload = []
-    if args.name:
-        payload = jsonKey2Influx(args.key, clientMemcached, args.name)
-    else:
-        payload = jsonKey2Influx(args.key, clientMemcached, args.key)
-    clientInflux.write_points(payload)
+    refreshRate = 5
+    if type(args.rate) == 'int':
+        refreshRate = args.rate
+    while True:
+        payload = []
+        if args.name:
+            payload = jsonKey2Influx(args.key, clientMemcached, args.name)
+        else:
+            payload = jsonKey2Influx(args.key, clientMemcached, args.key)
+        print('Publishing data')
+        print(payload)
+        #clientInflux.write_points(payload)
+        time.sleep(refreshRate)
 
 elif args.file:
     try:
@@ -112,7 +124,7 @@ elif args.file:
                         payload = jsonData2Influx(fileData,clientMemcached)
                         print (payload)
                         print ('\n')
-                        clientInflux.write_points(payload)
+                        #clientInflux.write_points(payload)
     except FileNotFoundError as e:
         sys.exit("Error: " + args.iptable + " is not valid or does not point to a DBFile.")
     print("program ended succesfully")
